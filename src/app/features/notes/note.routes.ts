ed006379e432e7bd6b11005a860f4299f8973268
umpierres <alexandraumpierres@outlook.com>
2023-10-07 18:02:32 -0300

    voltando o parametro para o get

--- src/app/features/notes/note.routes.ts
+++ src/app/features/notes/note.routes.ts
@@ -7,7 +7,7 @@ const routerNote = express.Router();
 const noteController = new NoteController();
 
 routerNote.post("/", validateIDUser, validateCreateNote, noteController.create)
-routerNote.get("/", validateIDUser, noteController.listNotes)
+routerNote.get("/:ownerID", validateIDUser, noteController.listNotes)
 routerNote.put("/:noteID", validateIDUser, validateUpdateNote, noteController.update)
 routerNote.put("/:noteID/archive", validateIDUser, noteController.archive)
 routerNote.put("/:noteID/favorite", validateIDUser, noteController.favorite)
